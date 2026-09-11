1aaeb3da1eb3ab04ea93d05123bcb6060c3510cf
habibaa001 <rashidhbaiba944@gmail.com>
2026-09-11 21:12:59 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h2 className="text-5xl text-red-600">Dev Stack Builder</h2>
+      <button className="btn btn-error">Error</button>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/nav.tsx
@@ -0,0 +1,12 @@
+import React from "../assets/logo-text.png";
+
+const nav = () => {
+    return (
+        <nav className = "bg-white-100">
+            <img src={React} alt="Logo" />
+
+        </nav>
+    );
+};
+
+export default nav;
