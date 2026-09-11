eb57f36d83340c8703486709a67ba15e822e8468
habibaa001 <rashidhbaiba944@gmail.com>
2026-09-11 22:08:48 +0600

    Added Navbar section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-
+import Nav from "./components/Nav";
 
 function App() {
 
   return (
     <>
-      <h2 className="text-5xl text-red-600">Dev Stack Builder</h2>
-      <button className="btn btn-error">Error</button>
+    <Nav />
+    
     </>
   )
 }
--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,12 +1,23 @@
-import React from "../assets/logo-text.png";
+import Logo from "../assets/logo-text.png";
 
-const nav = () => {
-    return (
-        <nav className = "bg-white-100">
-            <img src={React} alt="Logo" />
-
-        </nav>
-    );
+const Nav = () => {
+  return (
+    <nav className=" bg-white ">
+      <div className="container mx-auto flex justify-between">
+        <img src={Logo} alt="Logo" />
+        <ul className="flex gap-6 items-center">
+          <li>Home</li>
+          <li>Technologies</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+        <ul className="flex gap-4 items-center" >
+          <button className="btn btn-signIn">Sign In</button>
+          <button className="btn btn-signUp">Sign Up</button>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
-export default nav;
+export default Nav;
